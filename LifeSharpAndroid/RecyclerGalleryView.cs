35358d12928784eeffb5199a9959f04e2dbdb87f
Deciare <deciare@isisview.org>
2016-07-27 03:50:02 -0400

    Refactor threading and memory handling from UserGalleryAdapter into separate base class
    - Don't use a WeakReference for the ImageView in ViewHolder.SetImageViewTarget(); a strong reference to the ImageView already exists as a member variable of ViewHolder.
    - Confirm that the decoded bitmap hasn't already been recycled in another thread before setting an ImageView to display it.
    -

---
+++ LifeSharpAndroid/RecyclerGalleryView.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+using Android.Support.V7.Widget;
+
+namespace LifeSharp
+{
+
+public class RecyclerGalleryView
+{
+	public abstract class Adapter : RecyclerView.Adapter
+	{
+		const string LogTag = "Gallery.Adapter";
+
+		protected Activity activity;
+		protected int screenWidth;
+		protected List<ViewHolder> viewHolders;
+
+		public Adapter(Activity activity)
+		{
+			this.activity = activity;
+			viewHolders = new List<ViewHolder>();
+
+			// The target width of each displayed image shoud be proportional to
+			// a given percentage of the screen size.
+			IWindowManager wm = activity.GetSystemService(Context.WindowService).JavaCast<IWindowManager>();
+			Display display = wm.DefaultDisplay;
+			Point size = new Point();
+			display.GetSize(size);
+			screenWidth = size.X;
+		}
+
+		public void FreeAllBitmaps()
+		{
+			if (viewHolders.Count == 0)
+			{
+				Log.Warn(LogTag, "viewHolders list is empty. Is OnCreateViewHolder() appending to the list?");
+			}
+
+			// Iterate through all ViewHolders created by this adapter, and free the
+			// bitmap that was allocated by each.
+			foreach (ViewHolder holder in viewHolders)
+			{
+				holder.FreePreviousBitmap();
+			}
+		}
+	}
+
+
+	public abstract class ViewHolder : RecyclerView.ViewHolder
+	{
+		const string LogTag = "Gallery.ViewHolder";
+
+		protected Activity activity;
+		protected int targetWidth;
+		protected WeakReference<Bitmap> bitmapReference;
+		protected ImageView imageView;
+
+		protected ViewHolder(Activity activity, View itemView, int targetWidth) : base(itemView)
+		{
+			this.activity = activity;
+			this.targetWidth = targetWidth;
+		}
+
+		protected int CalculateInSampleSize(BitmapFactory.Options options)
+		{
+			int width = options.OutWidth / 2;
+			int inSampleSize = 1;
+
+			// Iterate until we find a power of 2 that, when dividing the bitmap's
+			// width by that value, would cause the resultant width to be smaller
+			// than the target width. Stop just before that happens.
+			while (width / inSampleSize > targetWidth)
+			{
+				inSampleSize *= 2;
+			}
+
+			return inSampleSize;
+		}
+
+		protected Bitmap GetBitmapForImage(Image image)
+		{
+			// Obtain dimensions of image.
+			var options = new BitmapFactory.Options();
+			options.InJustDecodeBounds = true;
+			BitmapFactory.DecodeFile(image.sourcePath, options);
+
+			// Generate a scaled-down bitmap of the image to put in the view.
+			options.InJustDecodeBounds = false;
+			options.InSampleSize = CalculateInSampleSize(options);
+			Bitmap bitmap = BitmapFactory.DecodeFile(image.sourcePath, options);
+
+			// Save a reference to the bitmap so it can be manually recycled later.
+			bitmapReference = new WeakReference<Bitmap>(bitmap);
+
+			return bitmap;
+		}
+
+		public void FreePreviousBitmap()
+		{
+			Debug.Assert(imageView != null, "imageView must be set by subclass constructor");
+
+			// If a bitmap was previously allocated for this view holder, free
+			// the memory that was used by that bitmap.
+			Bitmap bitmap;
+			if (bitmapReference != null && bitmapReference.TryGetTarget(out bitmap))
+			{
+				imageView.SetImageBitmap(null);
+				try
+				{
+					bitmap.Recycle();
+				}
+				catch (ObjectDisposedException e)
+				{
+					// In some cases, the bitmap was already garbage collected
+					// by the time we tried to manually free it.
+					Log.Warn(LogTag, "Bitmap was already freed: {0}", e);
+				}
+			}
+		}
+
+		public void SetImageViewTarget(Image image)
+		{
+			// When a different image is assigned to this view holder, the
+			// previous image's bitmap may not be freed immediately, resulting
+			// in the possibility of an out-of-memory error if SetImage() is
+			// called rapidly. Free the memory used by the previous image before
+			// attempting to allocate a bitmap for the next image.
+			FreePreviousBitmap();
+
+			// Decode image into bitmap in separate thread, to avoid locking up
+			// the UI thread.
+			ThreadPool.QueueUserWorkItem(delegate {
+				Bitmap bitmap = GetBitmapForImage(image);
+
+				// Once the bitmap decode is complete, assign it to the ImageView.
+				// This must be done on the UI thread.
+				activity.RunOnUiThread(delegate {
+					// When scrolling rapidly, it's possible that the bitmap has
+					// already been recycled by a subsequent call to
+					// SetImageViewTarget() before we get around to displaying
+					// it. Ensure that it hasn't been.
+					if (bitmap != null && !bitmap.IsRecycled)
+					{
+						// Update views based on image properties.
+						imageView.SetImageBitmap(bitmap);
+					}
+				});
+			});
+		}
+	}
+}
+
+}
--- LifeSharpAndroid/UserGalleryActivity.cs
+++ LifeSharpAndroid/UserGalleryActivity.cs
@@ -8,15 +8,13 @@
 
 using System;
 using System.Collections.Generic;
-using System.Threading;
 
 using Android.App;
 using Android.Content;
-using Android.Graphics;
 using Android.OS;
-using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+
 using Android.Support.V7.App;
 using Android.Support.V7.Widget;
 
@@ -25,139 +23,46 @@ using Toolbar = Android.Support.V7.Widget.Toolbar;
 namespace LifeSharp
 {
 
-public class UserGalleryAdapter : RecyclerView.Adapter
+public class UserGalleryAdapter : RecyclerGalleryView.Adapter
 {
-	Activity _activity;
 	List<Image> _images;
 	ImageDatabaseAndroid _db;
-	List<ViewHolder> _viewHolders;
 
-	public class ViewHolder : RecyclerView.ViewHolder
+	public class ViewHolder : RecyclerGalleryView.ViewHolder
 	{
-		Activity _activity;
-		int _targetWidth;
-		WeakReference<Bitmap> _bitmapReference;
 		public Image image { get; private set; }
-		public DynamicImageView imageView { get; private set; }
 		public TextView badgesView { get; private set; }
 		public TextView timeView { get; private set; }
 
-		public ViewHolder(Activity activity, RelativeLayout itemView, EventHandler<int> onClick) : base(itemView)
+		public ViewHolder(Activity activity, RelativeLayout itemView, int targetWidth, EventHandler<int> onClick) : base(activity, itemView, targetWidth)
 		{
-			_activity = activity;
-
 			// Clicking on the item view triggers the adapter's Click event.
 			itemView.Click += delegate {
 				onClick(this, image.id);
 			};
 
 			// Attach member variables to child views of this item view.
-			imageView = itemView.FindViewById<DynamicImageView>(Resource.Id.galleryTileImage);
+			imageView = itemView.FindViewById<ImageView>(Resource.Id.galleryTileImage);
 			badgesView = itemView.FindViewById<TextView>(Resource.Id.galleryTileBadges);
 			timeView = itemView.FindViewById<TextView>(Resource.Id.galleryTileTime);
-
-			// UserGalleryActivity shows 2 columns, so the target size should be half the width of the display.
-			IWindowManager wm = activity.GetSystemService(Context.WindowService).JavaCast<IWindowManager>();
-			Display display = wm.DefaultDisplay;
-			Point size = new Point();
-			display.GetSize(size);
-			_targetWidth = size.X / 2;
-		}
-
-		public int CalculateInSampleSize(BitmapFactory.Options options)
-		{
-			int width = options.OutWidth / 2;
-			int inSampleSize = 1;
-
-			// Iterate until we find a power of 2 that, when dividing the bitmap's
-			// width by that value, would cause the resultant width to be smaller
-			// than the target width. Stop just before that happens.
-			while (width / inSampleSize > _targetWidth)
-			{
-				inSampleSize *= 2;
-			}
-
-			return inSampleSize;
-		}
-
-		public void FreePreviousBitmap()
-		{
-			// If a bitmap was previously allocated for this view holder, free
-			// the memory that was used by that bitmap.
-			Bitmap bitmap;
-			if (_bitmapReference != null && _bitmapReference.TryGetTarget(out bitmap))
-			{
-				imageView.SetImageBitmap(null);
-				try
-				{
-					bitmap.Recycle();
-				}
-				catch (ObjectDisposedException e)
-				{
-					// In some cases, the bitmap was already garbage collected
-					// by the time we tried to manually free it.
-					Log.Warn("UserGalleryAdapter", "Bitmap was already freed: {0}", e);
-				}
-			}
 		}
 
 		public void SetImage(Image image)
 		{
-			// When a different image is assigned to this view holder, the
-			// previous image's bitmap may not be freed immediately, resulting
-			// in the possibility of an out-of-memory error if SetImage() is
-			// called rapidly. Free the memory used by the previous image before
-			// attempting to allocate a bitmap for the next image.
-			FreePreviousBitmap();
+			// Base class handles decoding and assigning image to view.
+			SetImageViewTarget(image);
 
-			// Decode image into bitmap in separate thread, to avoid locking up
-			// the UI thread.
-			ThreadPool.QueueUserWorkItem(delegate(object state) {
-				this.image = image;
-
-				// Obtain dimensions of image.
-				var options = new BitmapFactory.Options();
-				options.InJustDecodeBounds = true;
-				BitmapFactory.DecodeFile(image.sourcePath, options);
-
-				// Generate a scaled-down bitmap of the image to put in the view.
-				options.InJustDecodeBounds = false;
-				options.InSampleSize = CalculateInSampleSize(options);
-				// Use a weak reference to avoid preventing the ImageView from
-				// being garbage collected if the view becomes no longer needed
-				// while the bitmap is still becoming decoded.
-				var viewReference = new WeakReference<DynamicImageView>(imageView);
-				Bitmap bitmap = BitmapFactory.DecodeFile(image.sourcePath, options);
-
-				// Once the bitmap decode is complete, assign it to the ImageView.
-				// This must be done on the UI thread.
-				_activity.RunOnUiThread(delegate() {
-					// Update views based on image properties.
-					if (viewReference != null && bitmap != null)
-					{
-						// If the reference to the image view is still valid, then
-						// other views in this view holder are also still valid.
-						// Assign appropriate values to all views in this vie wholder.
-						DynamicImageView view;
-						if (viewReference.TryGetTarget(out view))
-						{
-							_bitmapReference = new WeakReference<Bitmap>(bitmap);
-							view.SetImageBitmap(bitmap);
-							badgesView.Text = image.comment.IsNullOrEmpty() ? "" : "💬";
-							timeView.Text = image.queueStamp.ToLocalTime().ToString("G");
-						}
-					}
-				});
-			});
+			// Additional things to be done for this class.
+			this.image = image;
+			badgesView.Text = image.comment.IsNullOrEmpty() ? "" : "💬";
+			timeView.Text = image.queueStamp.ToLocalTime().ToString("G");
 		}
 	}
 
-	public UserGalleryAdapter(Activity activity, string user)
+	public UserGalleryAdapter(Activity activity, string user) : base(activity)
 	{
-		_activity = activity;
 		_db = ImageDatabaseAndroid.GetSingleton(activity);
 		_images = _db.getImagesByUser(user);
-		_viewHolders = new List<ViewHolder>();
 	}
 
 	public override RecyclerView.ViewHolder OnCreateViewHolder(ViewGroup parent, int viewType)
@@ -168,8 +73,8 @@ public class UserGalleryAdapter : RecyclerView.Adapter
 		// Create a ViewHolder based on the inflated XML layout.
 		// Maintain a list of all ViewHolders that are created by this adapter,
 		// so that bitmaps allocated in the ViewHolder can be freed.
-		var holder = new ViewHolder(_activity, (RelativeLayout)view, Click);
-		_viewHolders.Add(holder);
+		var holder = new ViewHolder(activity, (RelativeLayout)view, (int)(screenWidth * 0.5), Click);
+		viewHolders.Add(holder);
 		return holder;
 	}
 
@@ -184,16 +89,6 @@ public class UserGalleryAdapter : RecyclerView.Adapter
 		get
 		{
 			return _images.Count;
-		}
-	}
-
-	public void FreeAllBitmaps()
-	{
-		// Iterate through all ViewHolders created by this adapter, and free the
-		// bitmap that was allocated by each.
-		foreach (ViewHolder holder in _viewHolders)
-		{
-			holder.FreePreviousBitmap();
 		}
 	}
 
